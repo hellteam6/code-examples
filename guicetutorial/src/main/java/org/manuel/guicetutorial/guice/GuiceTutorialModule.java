066346edb264350982f3cef5f1802950ab8cb2bf
Manuel <manu.2ka8@gmail.com>
2013-02-19 00:22:38 +0100

    example of provider method and class

--- guicetutorial/src/main/java/org/manuel/guicetutorial/guice/GuiceTutorialModule.java
+++ guicetutorial/src/main/java/org/manuel/guicetutorial/guice/GuiceTutorialModule.java
@@ -19,6 +19,36 @@ public class GuiceTutorialModule extends AbstractModule {
 		bind(Battery.class).to(LiIonBattery.class);	
 		bind(CombustionEngine.class).to(V8Engine.class);
 		bind(Turbo.class).to(PremiumTurbo.class);
+		//We can bind classes to providers if we need complex creation logic
+//		bind(InternalCombustionCar).toProvider(InternalCombustionCarProvider.class);
 	}
+	
+	//Example of a Guice style factory or provider method. For these methods
+	//we don't need to specify a binding, guice finds the return type by introspection and does
+	//the binding for us.
+//	@Provides
+//	InternalCombustionCar provideInternalCombustionCar() {
+//		CombustionEngine engine = new V8Engine(new PremiumTurbo());
+//		InternalCombustionCar car = new InternalCombustionCar(engine);
+//		return car;
+//	}
+
+	//Example of a Guice style factory or provider
+//	class InternalCombustionCarProvider implements Provider<InternalCombustionCar> {
+//
+//		private final CombustionEngine engine;
+//
+//		@Inject
+//		public InternalCombustionCarProvider(CombustionEngine engine) {
+//			this.engine = engine;
+//		}
+//
+//		public InternalCombustionCar get() {
+//			InternalCombustionCar car = new InternalCombustionCar(engine);
+//			car.setComplicatedStuff1();
+//			car.doComplexInitializationStuff();
+//			return car;
+//		}
+//	}
 
 }
